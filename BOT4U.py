6a96556461e432127480ed47e93d732a939a4abb
parvathi25 <parvathivinod2506@gmail.com>
2024-06-23 18:28:19 +0530

    version 2

--- BOT4U.py
+++ BOT4U.py
@@ -1,31 +1,20 @@
 import streamlit as st
-from transformers import pipeline
 from langchain_community.llms import HuggingFaceEndpoint
 import os
 
-# Instantiate the sentiment analysis pipeline
-classifier = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
-# Define a mapping dictionary for sentiment labels
-label_map = {
-    '5 stars': 'Positive',
-    '4 stars': 'Positive',
-    '3 stars': 'Neutral',
-    '2 stars': 'Negative',
-    '1 star': 'Negative'
-}
-
 # Set your Hugging Face API token
-api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+api_key = os.getenv("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")
 # Ensure API key is set properly
 if not api_key:
     raise ValueError("Hugging Face API token not found. Set the environment variable 'HUGGINGFACEHUB_API_TOKEN'.")
 
 repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
-# Create the language model endpoint
+
+# Create the language model endpoint for Mistral 7B
 llm = HuggingFaceEndpoint(
     repo_id=repo_id,
     temperature=0.9,
-    max_new_tokens=500
+    max_new_tokens=700
 )
 
 st.title('BOT4U')
@@ -46,13 +35,3 @@ elif selected_section == "Sentiment Analysis":
     st.header('Sentiment Analysis')
 
     sentence = st.text_area("Enter a sentence for sentiment analysis")
-
-    if st.button("Analyze"):
-        if sentence:
-            # Perform sentiment analysis
-            result = classifier(sentence)
-            # Extract sentiment label and map to 'Positive' or 'Negative'
-            sentiment_label = result[0]['label']
-            sentiment = label_map.get(sentiment_label, 'Unknown')
-            # Display result
-            st.write(f"The sentiment of the sentence '{sentence}' is: {sentiment}")
